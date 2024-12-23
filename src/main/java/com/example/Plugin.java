534de22d120a1b1f62c0856d9959c967ac6725c2
Quang <qth9368@rit.edu>
2024-12-23 10:57:37 -0500

    initial

---
+++ src/main/java/com/example/Plugin.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import java.util.logging.Logger;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/*
+ * gitpush java plugin
+ */
+public class Plugin extends JavaPlugin
+{
+  private static final Logger LOGGER=Logger.getLogger("gitpush");
+
+  public void onEnable()
+  {
+    LOGGER.info("gitpush enabled");
+  }
+
+  public void onDisable()
+  {
+    LOGGER.info("gitpush disabled");
+  }
+}
